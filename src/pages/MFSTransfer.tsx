fe7428e7e3331e2b906f7bfc8169d5f611d5a510
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-05-10 02:40:51 +0600

    fix: tailwind  config plugins

--- src/pages/MFSTransfer.tsx
+++ src/pages/MFSTransfer.tsx
@@ -48,7 +48,7 @@ const MFSTransfer = () => {
             key={mfs.name}
             className={`flex items-center justify-center py-4 rounded-lg shadow-md transition-transform duration-300 ${
               selectedMFS === mfs.name
-                ? 'bg-gray-300' // Light gray for selected
+                ? 'bg-gray-200' // Light gray for selected
                 : 'bg-white hover:scale-105 hover:shadow-lg' // Hover effect for unselected
             }`}
             onClick={() => setSelectedMFS(mfs.name)}
